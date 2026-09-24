a1b87fb05e645f77667bdaf433e878c41b37650a
Akashr241 <akashr.offical7@gmail.com>
2026-09-24 18:36:11 +0530

    Enable orphan removal for cart items in Cart entity

--- E-commerce/src/main/java/com/example/demo/cart/entity/Cart.java
+++ E-commerce/src/main/java/com/example/demo/cart/entity/Cart.java
@@ -24,6 +24,7 @@ public class Cart {
     @OneToMany(
             mappedBy = "cart",
             cascade = CascadeType.ALL,
+            orphanRemoval = true,
             fetch = FetchType.EAGER)
      @JsonBackReference       
     private List<CartItem> cartItems =
